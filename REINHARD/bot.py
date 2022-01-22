25aaaff3c0728f31d7438314da2376052e3ff33f
AttractiveToad <alois.zimmermann2003@gmail.com>
2022-01-22 22:52:52 +0100

    create first function

--- REINHARD/bot.py
+++ REINHARD/bot.py
@@ -1,10 +1,7 @@
-from audioop import add
 import os
-
 import hikari
 import lightbulb
-
-from REINHARD.constants import MODERATOR_ROLE_NAME, TOKEN, DEFAULT_GUILD_ID, STDOUT_CHANNEL_ID
+from REINHARD.constants import TOKEN, DEFAULT_GUILD_ID, STDOUT_CHANNEL_ID
 
 bot = lightbulb.BotApp(
    token = TOKEN,
@@ -15,19 +12,18 @@ bot = lightbulb.BotApp(
    case_insensitive_prefix_commands=True
 )
 
-@bot.listen(hikari.StartedEvent)
-async def on_starting(event: hikari.StartedEvent) -> None:
-   print("Im starting")
+@bot.listen(hikari.GuildMessageCreateEvent)
+async def print_message(event) -> None:
+   print(event.content)
 
 @bot.listen(hikari.StartedEvent)
-async def on_started(event: hikari.StartedEvent) -> None:
+async def on_started(event) -> None:
    channel = await bot.rest.fetch_channel(STDOUT_CHANNEL_ID)
    await channel.send("online")
    print("Im started")
 
 @bot.command()
 @lightbulb.add_cooldown(5.0, 1, lightbulb.UserBucket)
-#@lightbulb.add_checks(lightbulb.owner_only, lightbulb.has_roles(601422939765604400))
 @lightbulb.option("text", "The thing to say.")
 @lightbulb.command("say", "Make the bot say something.")
 @lightbulb.implements(lightbulb.SlashCommand, lightbulb.PrefixCommand)
@@ -39,5 +35,3 @@ def run() -> None:
       import uvloop
       uvloop.install()
    bot.run()
-
-    
