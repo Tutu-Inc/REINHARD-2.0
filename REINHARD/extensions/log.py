fd97b5314b83f2fe6f669aa44b278117fef909a8
Alois Zimmermann <alois.zimmermann2003@gmail.com>
2022-02-19 14:17:19 +0100

    update

--- REINHARD/extensions/log.py
+++ REINHARD/extensions/log.py
@@ -4,12 +4,13 @@ from data import TOKEN, DEFAULT_GUILD_ID, STDOUT_CHANNEL_ID, Style
 
 log_plugin = lightbulb.Plugin("Log")
 
+#============LOG-MESSAGE============
 @log_plugin.listener(hikari.GuildMessageCreateEvent)
 async def print_message(event: hikari.GuildMessageCreateEvent) -> None:
    if event.is_bot or not event.content:
       return
    else:
-      print(Style.YELLOW + str(event.author) + ' wrote ' + event.content)
+      print(Style.YELLOW + str(event.author) + ' sent ' + event.content)
 
 def load(bot: lightbulb.BotApp) -> None:
    bot.add_plugin(log_plugin)
